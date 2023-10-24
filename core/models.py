eb5bbfa22622bdf1d8e3992bbc5f6530bc261c0a
EmersonSantosO <103337805+EmersonSantosO@users.noreply.github.com>
2023-10-24 19:35:36 -0300

    Created Models

--- core/models.py
+++ core/models.py
@@ -7,3 +7,28 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Country(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Country ID')
+    name = models.CharField(max_length=50, verbose_name='Country Name')
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Author Name')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='Country Author', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    isbn = models.CharField(max_length=10, verbose_name='ISBN', primary_key=True)
+    title = models.CharField(max_length=50, verbose_name='Title')
+    year = models.DateField(verbose_name='Year Published')
+    description = models.TextField(verbose_name='Description')
+    category = models.ManyToManyField(Category, verbose_name='Category')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='Author', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
