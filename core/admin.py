54be3b5fbe7bb20f6034df137cc2676577d2b2c3
EmersonSantosO <103337805+EmersonSantosO@users.noreply.github.com>
2023-10-25 23:41:31 -0300

    Commit Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Country, Author, Book
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('id')
+
+class CountryAdmin(admin.ModelAdmin):
+    readonly_fields = ('id')
+
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Country)
